8fb64b48d000313ff7e5e3815bb6333243f70443
jihun <jihun315016@gmail.com>
2023-09-23 16:28:30 +0900

    init

---
+++ PizzaApp/Data/PizzaContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaApp.Models;
+
+namespace PizzaApp.Data
+{
+    // DbContext : 데이터베이스와 상호 작용할 수 있는 게이트웨이
+    public class PizzaContext : DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options) { }
+
+
+        // DbSet<T> : 데이터베이스에 생성될 테이블
+        // 테이블 이름은 PizzaContext 클래스의 DbSet<T> 속성과 일치한다.
+        public DbSet<Pizza> Pizzas => Set<Pizza>();
+
+        public DbSet<Topping> Toppings => Set<Topping>();
+
+        public DbSet<Sauce> Sauces => Set<Sauce>();
+    }
+}
---
+++ PizzaApp/Program.cs
@@ -0,0 +1,47 @@
+    using PizzaApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+
+// new input START *****
+// Add the PizzaContext
+// AddSqlite<PizzaContext> : SQLite Provider를 사용하도록 지정하고, PizzaContext 객체를 의존성 주입 컨테이너에 등록
+// 등록된 PizzaContext 인스턴스는 필요로 하는 클래스나 서비스에 주입된다.
+// -> 따라서 해당 클래스나 서비스에서 PizzaContext 인스턴스를 직접 생성하거나 관리할 필요 없이 이미 준비된 PizzaContext 인스턴스를 사용하여 작업할 수 있다.
+// -> 요약 : 데이터베이스에 작업할 수 있는 PizzaContext 인스턴스를 만들어서 그 객체가 필요한 곳에 자동으로 제공해주겠다는 의미
+// Data Source=ContosoPizza.db : SQLite 데이터베이스 연결 문자열 -> 데이터베이스 파일인 ContosoPizza.db과 연결
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
+
+
+
+// Add the PromotionsContext
+// new input END *****
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
