17aa8e6d917c710ca600e276bc90e9eaed75e354
jihun <jihun315016@gmail.com>
2023-10-06 00:59:09 +0900

    PizzaService - temp

--- PizzaApp/Services/PizzaService.cs
+++ PizzaApp/Services/PizzaService.cs
@@ -34,23 +34,56 @@ public class PizzaService
             .SingleOrDefault(p => p.Id == id);
     }
 
-    public Pizza? Create(Pizza newPizza)
+    public Pizza Create(Pizza newPizza)
     {
-        throw new NotImplementedException();
+        _context.Pizzas.Add(newPizza);
+        _context.SaveChanges();
+
+        return newPizza;
     }
 
     public void AddTopping(int PizzaId, int ToppingId)
     {
-        throw new NotImplementedException();
+        var pizzaToUpdate = _context.Pizzas.Find(PizzaId);
+        var toppingToAdd = _context.Toppings.Find(ToppingId);
+
+        if (pizzaToUpdate is null || toppingToAdd is null)
+        {
+            throw new InvalidOperationException("Pizza or topping does not exist");
+        }
+
+        if (pizzaToUpdate.Toppings is null)
+        {
+            pizzaToUpdate.Toppings = new List<Topping>();
+        }
+
+        pizzaToUpdate.Toppings.Add(toppingToAdd);
+        _context.SaveChanges();
     }
 
     public void UpdateSauce(int PizzaId, int SauceId)
     {
-        throw new NotImplementedException();
+        // PizzaId 피자의 소스를 SauceId로 변경
+        var pizzaToUpdate = _context.Pizzas.Find(PizzaId);
+        var sauceToUpdate = _context.Sauces.Find(SauceId);
+
+        if (pizzaToUpdate is null || sauceToUpdate is null)
+        {
+            // 유효하지 않을 때 발생하는 예외
+            throw new InvalidOperationException("Pizza or sauce does not exist");
+        }
+
+        pizzaToUpdate.Sauce = sauceToUpdate;
+        _context.SaveChanges();
     }
 
     public void DeleteById(int id)
     {
-        throw new NotImplementedException();
+        var pizzaToDelete = _context.Pizzas.Find(id);
+        if (pizzaToDelete is not null)
+        {
+            _context.Pizzas.Remove(pizzaToDelete);
+            _context.SaveChanges();
+        }
     }
 }
